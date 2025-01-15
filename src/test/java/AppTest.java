d1f5acf9b2813811fe78662ec890a70190900e17
Vicent Roselló <vicentxeraco87@gmail.com>
2025-01-15 19:01:29 +0100

    commit correcció test

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,9 +1,10 @@
-import static org.junit.Assert.assertEquals;
-
+import org.junit.Test;
+import static org.junit.Assert.*;
 import com.example.Application;
 
 public class AppTest {
 
+    @Test
     public void testApp() {
         Application myApp = new Application();
         String result = myApp.getStatus();
